8852af0f5d6ba48e3625f0d75b311c2b1627e726
JUAN <https://github.com/JUAN1324JOSE/portafolio-JUAN-JOSE-PARRA.git>
2024-10-16 21:38:39 -0500

    se realiza la landing page

---
+++ java.js
@@ -0,0 +1,9 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function initMap() {
+    var location = { lat: -25.363, lng: 131.044 }; // Ubicación inicial
+    var map = new google.maps.Map(document.getElementById)('map') }
+        zoom
+        
